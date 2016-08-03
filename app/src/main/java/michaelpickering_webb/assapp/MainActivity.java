bf1eadb03acf46ab8aedc76276a33ebd8b4523e6
Michael Pickering-Webb <michael.pickering-we@motuekahigh.school.nz>
2016-08-04 07:58:40 +1200

    Initial commit

---
+++ app/src/main/java/michaelpickering_webb/assapp/MainActivity.java
@@ -0,0 +1,22 @@
+package michaelpickering_webb.assapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(MainActivity.this,Quiz.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/michaelpickering_webb/assapp/Quiz.java
@@ -0,0 +1,24 @@
+package michaelpickering_webb.assapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Quiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+
+    public void CheckChecked(){
+
+        for(int i = 1; i <=4; i++){
+
+
+        }
+
+    }
+}
